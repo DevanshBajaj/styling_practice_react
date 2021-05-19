85e1ec1c7ba60acf51aa33d012a620b7d1984288
Devansh Bajaj <devansh21db.db@gmail.com>
2021-05-20 02:49:27 +0530

    better UI and  used styled components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import styled from 'styled-components';
 
-import './Button.css';
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #CE828E;
+  color: white;
+  background: #CE828E;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+  border-radius: 6px;
 
+  &:focus {
+  outline: none;
+
+  &:hover,
+  &:active {
+  background: #ca6c7a;
+  border-color: #ca6c7a;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+
+
+/*
 const Button = props => {
   return (
     <button type={props.type} className="button" onClick={props.onClick}>
@@ -9,5 +30,5 @@ const Button = props => {
     </button>
   );
 };
-
+*/
 export default Button;
